8b0787bc4c26463f1b6d58c0935b409c55bd52c4
Tim Schüssler <tim.schussler@elev.ga.ntig.se>
2026-05-04 12:36:26 +0200

    Fixat pom.xml för projektet och skapat förälder klass

---
+++ src/main/java/proj2/Template.java
@@ -0,0 +1,13 @@
+package proj2;
+    import com.google.gson.*;
+    import com.google.gson.reflect.TypeToken;
+    import java.lang.reflect.Type;
+    import kong.unirest.Unirest;
+    import kong.unirest.HttpResponse;
+    import kong.unirest.UnirestException;
+    import java.nio.file.*;
+    import java.io.IOException;
+    import java.util.ArrayList;
+public class Template {
+
+}
