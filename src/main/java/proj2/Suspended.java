2c5a95858370cc90deaa5cb0528aa1cceb45a863
Tim Schüssler <tim.schussler@elev.ga.ntig.se>
2026-05-06 10:14:24 +0200

    Lagt till klass för användare

---
+++ src/main/java/proj2/Suspended.java
@@ -0,0 +1,6 @@
+package proj2;
+
+public class Suspended {
+public String id;
+public String customer_id;
+}
---
+++ src/main/java/proj2/User.java
@@ -0,0 +1,12 @@
+package proj2;
+
+public class User{
+public String id;
+public String name;
+public String email;
+User(String id, String name, String email){
+    this.id = id;
+    this.name = name;
+    this.email = email;
+}
+}
