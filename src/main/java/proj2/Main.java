e994d15007a354e69a36d031f717b43c94bc74a2
Tim Schüssler <tim.schussler@elev.ga.ntig.se>
2026-05-18 12:38:51 +0200

    Lagt till en meny!!

--- src/main/java/proj2/Main.java
+++ src/main/java/proj2/Main.java
@@ -1,16 +1,35 @@
 package proj2;
-
 import kong.unirest.Unirest;
+
+import java.io.Console;
+import java.util.*;
+import java.util.Scanner;
+
 
 //*
 // Detta är huvuddelen för projektet
 // Tim TE23E */
 public class Main {
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+            Console cnsl = System.console();
+        System.out.println("""
+          Library!
+          1. Visitor mode
+          2. Librarian mode
+          3. Admin mode 
+          4. Exit     
+        """);       
+            String mode = cnsl.readLine();
+            if (mode == "4") {
+                System.out.println("EXITTING!!!!!!!!");
+            }
+
+
         // Testkod
     // Book Testing = new Book("123","Epic","Epicman","Epicgenre",123,true);
     //   System.out.println(Testing.checkAvailibility());
+
 
     }
     public void getBook(){
